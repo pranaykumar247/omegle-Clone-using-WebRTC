78255f2e3e4c43a226074c02ce8553e3f02bf6ef
Pranay Kumar <pranaykumar074@gmail.com>
2024-07-08 11:22:26 +0530

    BE and FE connected

--- backend/dist/managers/UserManager.js
+++ backend/dist/managers/UserManager.js
@@ -1,65 +1,47 @@
-import { Socket } from "socket.io";
-import { RoomManager } from "./RoomManager";
-
-
-export interface User {
-    socket: Socket;
-    name: String;
-}
-
-export class UserManager {
-    private users: User[];
-    private queue: string[];
-    private roomManager: RoomManager;
-
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.UserManager = void 0;
+const RoomManager_1 = require("./RoomManager");
+class UserManager {
     constructor() {
         this.users = [];
         this.queue = [];
-        this.roomManager = new RoomManager();
+        this.roomManager = new RoomManager_1.RoomManager();
     }
-
-    addUser(name: string, socket: Socket) {
+    addUser(name, socket) {
         this.users.push({ name, socket });
-
         this.queue.push(socket.id);
         socket.send("lobby");
-        this.clearQueue()
+        this.clearQueue();
         this.initHandlers(socket);
     }
-
-    removeUser(socketId: string) {
+    removeUser(socketId) {
         const user = this.users.find(x => x.socket.id === socketId);
         this.users = this.users.filter(x => x.socket.id !== socketId);
         this.queue = this.queue.filter(x => x === socketId);
     }
-
     clearQueue() {
         if (this.queue.length < 2) {
             return;
         }
-
         const id1 = this.queue.pop();
         const id2 = this.queue.pop();
-
-
         const user1 = this.users.find(x => x.socket.id === id1);
         const user2 = this.users.find(x => x.socket.id === id2);
-
         if (!user1 || !user2) {
             return;
         }
-        const room = this.roomManager.createRoom(user1, user2)
+        const room = this.roomManager.createRoom(user1, user2);
         this.clearQueue();
     }
-
     // sdp : Session Description Protocol
-    initHandlers(socket: Socket) {
-        socket.on("offer", ({ sdp, roomId }: { sdp: string, roomId: string }) => {
+    initHandlers(socket) {
+        socket.on("offer", ({ sdp, roomId }) => {
             this.roomManager.onOffer(roomId, sdp);
-        })
-        socket.on("answer", ({ sdp, roomId }: { sdp: string, roomId: string }) => {
+        });
+        socket.on("answer", ({ sdp, roomId }) => {
             this.roomManager.onAnswer(roomId, sdp);
-        })
+        });
     }
-
 }
+exports.UserManager = UserManager;
--- frontend/src/components/Room.tsx
+++ frontend/src/components/Room.tsx
@@ -1,15 +1,56 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useSearchParams } from "react-router-dom";
+import { Socket, io } from "socket.io-client";
+
+const URL = "http://localhost:3000";
 
 export const Room = () => {
     const [searchParams, setSearchParams] = useSearchParams();
     const name = searchParams.get('name');
+    const [lobby,setLobby] = useState(true);
+    const [socket, setSocket] = useState<null | Socket>(null);
+
 
     useEffect(() => {
         // logic to init user to the room
+        const socket = io(URL);
+        socket.on('send-offer', ({ roomId }) => {
+            alert("send offer please");
+            setLobby(false);
+            socket.emit("offer", {
+                sdp: "",
+                roomId
+            })
+        });
+        socket.on('offer', ({ roomId, offer }) => {
+            alert("send answer please");
+            setLobby(false);
+            socket.emit("answer", {
+                roomId,
+                sdp: ""
+            })
+        });
+        
+        socket.on('answer', ({ roomId, answer }) => {
+            alert("connection done");
+            setLobby(false);
+        });
+
+        socket.on("lobby", () => {
+            setLobby(true);
+        })
+
+        setSocket(socket)
     }, [name])
 
+    if (lobby) {
+        return <div>
+            Waiting to connect to someone
+        </div>
+    }
     return <div>
-        Hi {name}
+        Hi {name}!!
+        <video width = {400} height = {400} />
+        <video width = {400} height = {400} />
     </div>
 }
